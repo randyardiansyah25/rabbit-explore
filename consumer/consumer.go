a5c62ca515bbf37113fd845d7437c0491d345542
randyardiansyah25 <detwentyfive@gmail.com>
2024-10-15 17:38:48 +0700

    support delay

--- consumer/consumer.go
+++ consumer/consumer.go
@@ -8,9 +8,13 @@ import (
 )
 
 const (
-	QUEUE_NAME = "elastic"
-	// QUEUE_NAME = "database"
-	// QUEUE_NAME = ""
+	rabbit_url           = "amqp://guest:guest@192.169.253.156:5672/"
+	xchange_name         = "debug-x"
+	xchange_name_delayed = "debug-x-delayed"
+
+	QUEUE_NAME         = "debug-q"
+	QUEUE_DELAYED_NAME = "debug-q-delayed"
+	// QUEUE_DELAYED_NAME = ""
 )
 
 func failOnError(err error, msg string) {
@@ -20,11 +24,16 @@ func failOnError(err error, msg string) {
 }
 
 func main() {
+	// UseDefault()
+	UseDelayedQueue()
+}
+
+func UseDefault() {
 	exclusive := true
 	if QUEUE_NAME != "" {
 		exclusive = false
 	}
-	conn, err := amqp.Dial("amqp://randy:123456@localhost:5672/")
+	conn, err := amqp.Dial(rabbit_url)
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
@@ -32,31 +41,31 @@ func main() {
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
 
-	err = ch.ExchangeDeclare(
-		"transaction_log", // name
-		"fanout",          // type
-		true,              // durable
-		false,             // auto-deleted
-		false,             // internal
-		false,             // no-wait
-		nil,               // arguments
-	)
-	failOnError(err, "Failed to declare an exchange")
+	// err = ch.ExchangeDeclare(
+	// 	xchange_name, // name
+	// 	"fanout",          // type
+	// 	true,              // durable
+	// 	false,             // auto-deleted
+	// 	false,             // internal
+	// 	false,             // no-wait
+	// 	nil,               // arguments
+	// )
+	// failOnError(err, "Failed to declare an exchange")
 
 	q, err := ch.QueueDeclare(
 		QUEUE_NAME, // name
 		false,      // durable
 		false,      // delete when unused
-		exclusive,      // exclusive
+		exclusive,  // exclusive
 		false,      // no-wait
 		nil,        // arguments
 	)
 	failOnError(err, "Failed to declare a queue")
 
 	err = ch.QueueBind(
-		q.Name,            // queue name
-		"",                // routing key
-		"transaction_log", // exchange
+		q.Name,       // queue name
+		"",           // routing key
+		xchange_name, // exchange
 		false,
 		nil)
 	failOnError(err, "Failed to bind a queue")
@@ -77,8 +86,65 @@ func main() {
 	go func() {
 		for d := range msgs {
 			log.Printf("Received a message: %s", d.Body)
-			d.Reject(true)
-			// d.Nack(false, true)
+			//d.Reject(true)
+			d.Ack(false)
+			time.Sleep(5 * time.Second)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+}
+
+func UseDelayedQueue() {
+	exclusive := true
+	if QUEUE_DELAYED_NAME != "" {
+		exclusive = false
+	}
+	conn, err := amqp.Dial(rabbit_url)
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		QUEUE_DELAYED_NAME, // name
+		false,              // durable
+		false,              // delete when unused
+		exclusive,          // exclusive
+		false,              // no-wait
+		nil,                // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	err = ch.QueueBind(
+		q.Name,               // queue name
+		"",                   // routing key
+		xchange_name_delayed, // exchange
+		false,
+		nil)
+	failOnError(err, "Failed to bind a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		false,  // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+			//d.Reject(true)
+			d.Ack(false)
 			time.Sleep(5 * time.Second)
 		}
 	}()
--- publisher/main.go
+++ publisher/main.go
@@ -6,6 +6,12 @@ import (
 	"github.com/streadway/amqp"
 )
 
+const (
+	rabbit_url           = "amqp://guest:guest@192.169.253.156:5672/"
+	xchange_name         = "debug-x"
+	xchange_name_delayed = "debug-x-delayed"
+)
+
 func failOnError(er error, msg string) {
 	if er != nil {
 		log.Fatalf("%s %s", msg, er)
@@ -13,7 +19,11 @@ func failOnError(er error, msg string) {
 }
 
 func main() {
-	conn, er := amqp.Dial("amqp://randy:123456@localhost:5672/")
+	UseDelayedFanout()
+}
+
+func UseFanout() {
+	conn, er := amqp.Dial(rabbit_url)
 	failOnError(er, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
@@ -22,23 +32,66 @@ func main() {
 	defer ch.Close()
 
 	er = ch.ExchangeDeclare(
-		"transaction_log", // name
-		"fanout",          // type
-		true,              // durable
-		false,             // auto-deleted
-		false,             // internal
-		false,             // no-wait
-		nil,               // arguments
+		xchange_name, // name
+		"fanout",     // type
+		true,         // durable
+		false,        // auto-deleted
+		false,        // internal
+		false,        // no-wait
+		nil,          // arguments
 	)
 	failOnError(er, "Failed to declare an exchange")
 
 	body := "Hello World!"
 	er = ch.Publish(
-		"transaction_log", // exchange
-		"",                // routing key
-		false,             // mandatory
-		false,             // immediate
+		xchange_name, // exchange
+		"",           // routing key
+		false,        // mandatory
+		false,        // immediate
 		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(er, "Failed to publish a message")
+
+	log.Printf(" [x] Sent %s", body)
+}
+
+func UseDelayedFanout() {
+	conn, er := amqp.Dial(rabbit_url)
+	failOnError(er, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, er := conn.Channel()
+	failOnError(er, "Failed to open channel")
+	defer ch.Close()
+
+	er = ch.ExchangeDeclare(
+		xchange_name_delayed, // name
+		"x-delayed-message",  //** type : untuk menggunakan delay, type menggunakan x-delayed-message
+		true,                 // durable
+		false,                // auto-deleted
+		false,                // internal
+		false,                // no-wait
+		amqp.Table{ //** arguments : tambahkan argumen berikut untuk tipe fanout
+			"x-delayed-type": "fanout",
+		},
+	)
+	failOnError(er, "Failed to declare an exchange")
+
+	body := "Delay Message >> Hello World! "
+	//** Header untuk konfigurasi durasi delay, set 0 jika tidak menggunakan delay
+	headers := amqp.Table{
+		"x-delay": int32(5000), // Delay 5 detik (dalam milidetik)
+	}
+
+	er = ch.Publish(
+		xchange_name_delayed, // exchange
+		"",                   // routing key
+		false,                // mandatory
+		false,                // immediate
+		amqp.Publishing{
+			Headers:     headers, // Tambahkan header
 			ContentType: "text/plain",
 			Body:        []byte(body),
 		})
